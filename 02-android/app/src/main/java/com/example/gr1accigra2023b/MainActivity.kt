1cd98974fc8f482d222bbfe5678824b9b3d5a3db
isaac reyes <isaac.reyes@epn.edu.ec>
2024-01-10 20:15:34 -0500

    hasta crear eliminar actualizar

--- 02-android/app/src/main/java/com/example/gr1accigra2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accigra2023b/MainActivity.kt
@@ -60,8 +60,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        //Base de Datos SQLITE
+        EBaseDeDatos.tablaEntrenador=ESqliteHelperEntrenador(this)
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener {
@@ -90,6 +93,13 @@ class MainActivity : AppCompatActivity() {
                 abrirActividadConParametros(
                     CIntentExplicitoParametros::class.java)
             }
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
+
     } // Termina onCreate
     fun abrirActividadConParametros(
         clase: Class<*>
